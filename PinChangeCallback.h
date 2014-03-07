f178ed225ce99546ce40b3093840471098ddcb6f
tobiaspetschke <not@public.com>
2014-03-07 21:20:47 +0000

    Converted to lib

---
+++ PinChangeCallback.h
@@ -0,0 +1,13 @@
+/* Small Pin Change Lib based on Arduino PCIntLib */
+#ifndef __PINCHANGECALLBACK_H__
+#define __PINCHANGECALLBACK_H__
+
+#include "pins_arduino.h"
+
+typedef void (*pinChangeFunc)(uint8_t);
+
+/* Mode: RISING, FALLING, CHANGE */
+void PCattachInterrupt(uint8_t pin, pinChangeFunc func , int mode);
+void PCdetachInterrupt(uint8_t pin);
+
+#endif
